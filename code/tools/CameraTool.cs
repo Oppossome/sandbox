3919b195cfdfa5942e7c2df2c8fa40380f38d668
Opossome <dsecutler@gmail.com>
2022-02-12 18:06:04 -0500

    Improve notifications, add notification for cam tool

--- code/tools/CameraTool.cs
+++ code/tools/CameraTool.cs
@@ -10,6 +10,14 @@ namespace Sandbox.Tools
 	[Library( "tool_camera", Title = "Camera" )]
 	public class CameraTool : BaseTool
 	{
+		public override void Activate()
+		{
+			base.Activate();
+			if ( Host.IsServer ) return;
+			Notifications.Send( "UI hidden for screenshot purposes", 3 );
+			Notifications.Send( "Menus and tool switching will still work", 3 );
+		}
+
 		public override void Simulate()
 		{
 			if ( Parent.ViewModelEntity is ModelEntity ent )
